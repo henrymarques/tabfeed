780ba245442cf27ce39dce034fbfc2e1e16f1ebb
Henrique Marques <henrique13210@gmail.com>
2026-01-23 13:23:10 -0400

    add lint scripts

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 function Home() {
   return (
-    <h1>Mesmo sob cabeçadas e tabefes, eu te amo muito! Um beijo do seu marido. 😘</h1>
+    <h1>
+      Mesmo sob cabeçadas e tabefes, eu te amo muito! Um beijo do seu marido. 😘
+    </h1>
   );
 }
 
