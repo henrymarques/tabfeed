d8c7fbd622a2414ccce205e5be0dc4e061d3bfac
Henrique Marques <henrique13210@gmail.com>
2026-01-28 23:42:02 +0000

    finish /api/v1/status endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,29 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1;");
+  const updatedAt = new Date().toISOString();
 
-  response.status(200).json();
+  const queryServerVersion = await database.query("SHOW server_version;");
+  const queryMaxConnections = await database.query("SHOW max_connections;");
+  const queryOpenedConnections = await database.query({
+    text: "SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [process.env.POSTGRES_DB],
+  });
+
+  const serverVersion = queryServerVersion.rows[0].server_version;
+  const maxConnections = parseInt(queryMaxConnections.rows[0].max_connections);
+  const openedConnections = queryOpenedConnections.rows[0].count;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: serverVersion,
+        max_connections: maxConnections,
+        opened_connections: openedConnections,
+      },
+    },
+  });
 }
 
 export default status;
