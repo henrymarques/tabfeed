d041dd6c09bd1d358e676baf46801db24033d8ae
Henrique Marques <marques.hique@gmail.com>
2026-07-09 17:32:06 -0400

    style: change code styles

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,5 @@
 function Home() {
-  return (
-    <h1>
-      Mesmo sob cabeçadas e tabefes, eu te amo muito! Um beijo do seu marido. 😘
-    </h1>
-  );
+  return <h1>Mesmo sob cabeçadas e tabefes, eu te amo muito! Um beijo do seu marido. 😘</h1>;
 }
 
 export default Home;
