ce906220f3fdb21044e1268c88cf3a79dc74d395
Henrique Marques <marques.hique@gmail.com>
2026-07-23 18:30:38 -0400

    fix: add optional chaining in `orchestrator.createUser`

--- tests/orchestrator.js
+++ tests/orchestrator.js
@@ -29,9 +29,9 @@ async function runPendingMigrations() {
 
 async function createUser(userObject) {
   return await user.create({
-    username: userObject.username || faker.internet.username().replace(/[_.-]/g, ""),
-    email: userObject.email || faker.internet.email(),
-    password: userObject.password || "password",
+    username: userObject?.username || faker.internet.username().replace(/[_.-]/g, ""),
+    email: userObject?.email || faker.internet.email(),
+    password: userObject?.password || "password",
   });
 }
 
