0db34f4821a246b926012be6c3c1da36f899f0f6
Henrique Marques <marques.hique@gmail.com>
2026-07-25 17:50:00 -0400

    feat: add `features` column to `users` table

--- tests/integration/api/v1/users/post.test.js
+++ tests/integration/api/v1/users/post.test.js
@@ -37,6 +37,7 @@ describe("POST /api/v1/users", () => {
         id: responseBody.id,
         username: "usuario",
         email: "teste@teste.com",
+        features: [],
         password: responseBody.password,
         created_at: responseBody.created_at,
         updated_at: responseBody.updated_at,
