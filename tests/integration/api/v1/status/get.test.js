adc3560c840b419aca0a0c478e8d7a0b67d9227a
Henrique Marques <marques.hique@gmail.com>
2026-07-29 09:20:19 -0400

    test: add default user coverage in `GET /api/v1/status` tests

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -23,6 +23,26 @@ describe("GET /api/v1/status", () => {
     });
   });
 
+  describe("Default user", () => {
+    test("Retrieving current system status", async () => {
+      const testUser = await orchestrator.createUser();
+      await orchestrator.activateUser(testUser);
+      const testUserSession = await orchestrator.createSession(testUser);
+      const response = await fetch(`${webserver.origin}/api/v1/status`, {
+        headers: { cookie: `session_id=${testUserSession.token}` },
+      });
+
+      expect(response.status).toBe(200);
+
+      const responseBody = await response.json();
+      expect(responseBody.updated_at).toEqual(new Date(responseBody.updated_at).toISOString());
+
+      expect(responseBody.dependencies.database.version).toBeUndefined();
+      expect(responseBody.dependencies.database.max_connections).toEqual(100);
+      expect(responseBody.dependencies.database.opened_connections).toEqual(1);
+    });
+  });
+
   describe("Privileged user", () => {
     test("Retrieving current system status", async () => {
       const testUser = await orchestrator.createUser();
